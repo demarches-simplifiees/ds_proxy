74cdb4a9d6f9c44eed498da5ae879fa923b555c9
simon lehericey <mail@simon.lehericey.net>
2019-04-17 12:16:55 +0200

    Remove warning

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -1,6 +1,7 @@
 use sodiumoxide::crypto::secretstream::{Stream, Tag};
 use sodiumoxide::crypto::secretstream::xchacha20poly1305::{Key, Header};
 
+#[allow(dead_code)]
 pub fn encrypt_and_decrypt(key: Key, array: [u8; 1]) -> [u8; 1] {
     let (mut enc_stream, header) = Stream::init_push(&key).unwrap();
 
@@ -11,12 +12,14 @@ pub fn encrypt_and_decrypt(key: Key, array: [u8; 1]) -> [u8; 1] {
     [decrypted1[0]]
 }
 
+#[allow(dead_code)]
 pub fn encrypt(key: &Key, clear: [u8; 1]) -> (Vec<u8>, Header) {
     let (mut enc_stream, header) = Stream::init_push(key).unwrap();
 
     (enc_stream.push(&clear, None, Tag::Message).unwrap(), header)
 }
 
+#[allow(dead_code)]
 pub fn decrypt(key: &Key, header: &Header, encrypted: Vec<u8>) -> [u8; 1] {
     let mut dec_stream = Stream::init_pull(&header, key).unwrap();
 
