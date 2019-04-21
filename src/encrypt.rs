2184e6afc4b3779289db2ba618e3a868845614b0
simon lehericey <mail@simon.lehericey.net>
2019-04-21 23:30:30 +0200

    Encode a stream

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -1,6 +1,8 @@
-use sodiumoxide::crypto::secretstream::{Stream, Tag};
+use sodiumoxide::crypto::secretstream::{Tag};
 use sodiumoxide::crypto::secretstream::xchacha20poly1305::{Key, Header};
 use sodiumoxide::crypto::secretstream::xchacha20poly1305;
+use futures::stream;
+use futures::stream::Stream;
 
 #[allow(dead_code)]
 pub fn encrypt(enc_stream: &mut xchacha20poly1305::Stream<xchacha20poly1305::Push>, clear: &[u8]) -> Vec<u8> {
@@ -37,7 +39,7 @@ mod tests {
     fn test_encrypt_and_decrypt() {
         let key: Key = build_key();
 
-        let (mut enc_stream, header) = Stream::init_push(&key).unwrap();
+        let (mut enc_stream, header) = xchacha20poly1305::Stream::init_push(&key).unwrap();
         let mut target_file_bytes: Vec<u8> = header[0..].to_vec();
 
         let chunck_size = 2;
@@ -54,7 +56,53 @@ mod tests {
 
         let mut result: Vec<u8>  = [].to_vec();
 
-        let mut dec_stream = Stream::init_pull(&decrypted_header, &key).unwrap();
+        let mut dec_stream = xchacha20poly1305::Stream::init_pull(&decrypted_header, &key).unwrap();
+
+
+        cipher.chunks(xchacha20poly1305::ABYTES + chunck_size).for_each(|s| {
+            result.append(&mut decrypt(&mut dec_stream, s))
+        });
+
+        assert_eq!(source.to_vec(), result);
+    }
+
+    #[test]
+    fn test_encrypt_and_decrypt_stream() {
+        let key: Key = build_key();
+
+        let (mut enc_stream, header) = xchacha20poly1305::Stream::init_push(&key).unwrap();
+
+        let chunck_size = 2;
+
+        let source  = [22 as u8, 23 as u8, 24 as u8];
+
+        let stream = stream::iter_ok::<_, ()>(source.iter());
+
+        let header_bytes: Vec<u8> = header[0..].to_vec();
+
+        let header_stream = stream::once::<Vec<u8>, ()>(Ok(header_bytes));
+
+
+        use futures::future::Future;
+
+        let encoder = stream
+            .map(|slice: &u8| *slice)
+            .chunks(chunck_size)
+            .and_then(|slice: Vec<u8>| {
+                Ok(encrypt(&mut enc_stream, &slice))
+            });
+
+        let result_stream = header_stream.chain(encoder);
+
+        let target_bytes: Vec<u8> = result_stream.concat2().wait().unwrap();
+
+        let decrypted_header = Header::from_slice(&target_bytes[0..xchacha20poly1305::HEADERBYTES]).unwrap();
+
+        let cipher = &target_bytes[xchacha20poly1305::HEADERBYTES..];
+
+        let mut result: Vec<u8>  = [].to_vec();
+
+        let mut dec_stream = xchacha20poly1305::Stream::init_pull(&decrypted_header, &key).unwrap();
 
 
         cipher.chunks(xchacha20poly1305::ABYTES + chunck_size).for_each(|s| {
