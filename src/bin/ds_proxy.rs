632dda7b3b95da86e6893c17d2a4ab793dfa0f9c
clemkeirua <clement@keiruaprod.fr>
2019-05-14 07:46:47 +0200

    added the noop parameter

--- src/bin/ds_proxy.rs
+++ src/bin/ds_proxy.rs
@@ -7,15 +7,16 @@ const USAGE: &'static str = "
 DS encryption proxy.
 
 Usage:
-  ds_proxy encrypt <input-file> <output-file>
-  ds_proxy decrypt <input-file> <output-file>
-  ds_proxy proxy <listen-adress> <listen-port>
+  ds_proxy encrypt <input-file> <output-file> [--noop=<arg>]
+  ds_proxy decrypt <input-file> <output-file> [--noop=<arg>]
+  ds_proxy proxy <listen-adress> <listen-port> [--noop=<arg>]
   ds_proxy (-h | --help)
   ds_proxy --version
 
 Options:
   -h --help     Show this screen.
   --version     Show version.
+  --noop=<arg>  If true, will not do any encryption or decryption [default: false]
 ";
 
 #[derive(Debug, Deserialize)]
@@ -27,6 +28,13 @@ struct Args {
     cmd_encrypt: bool,
     cmd_decrypt: bool,
     cmd_proxy: bool,
+    flag_noop: String,
+}
+
+impl Args {
+    fn noop(&self) -> bool {
+        self.flag_noop == "true"
+    }
 }
 
 fn main() {
@@ -34,14 +42,17 @@ fn main() {
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
 
+    println!("{:#?} {}", args, args.noop());
+
+    let noop = args.noop();
     if args.cmd_proxy {
         let listen_adress = &args.arg_listen_adress.unwrap();
         let listen_port = args.arg_listen_port.unwrap();
         let upstream_base_url = "https://storage.gra5.cloud.ovh.net".to_string();
-        encrypt::proxy::main(listen_adress, listen_port, upstream_base_url).unwrap();
+        encrypt::proxy::main(listen_adress, listen_port, upstream_base_url, noop).unwrap();
     } else if args.cmd_encrypt {
-        encrypt::file::encrypt(args.arg_input_file.unwrap(), args.arg_output_file.unwrap());
+        encrypt::file::encrypt(args.arg_input_file.unwrap(), args.arg_output_file.unwrap(), noop);
     } else if args.cmd_decrypt {
-        encrypt::file::decrypt(args.arg_input_file.unwrap(), args.arg_output_file.unwrap());
+        encrypt::file::decrypt(args.arg_input_file.unwrap(), args.arg_output_file.unwrap(), noop);
     }
 }
--- src/proxy.rs
+++ src/proxy.rs
@@ -5,18 +5,24 @@ use actix_web::client::Client;
 use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use futures::Future;
 use actix_web::guard;
+use actix_web::http::Uri;
+
+fn create_url(base_url: &str, uri: &Uri) -> String {
+    format!("{}{}", base_url, uri)
+}
 
 fn forward(
     req: HttpRequest,
     payload: web::Payload,
     client: web::Data<Client>,
     upstream_base_url: web::Data<String>,
+    noop: web::Data<bool>,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
 
     let key = build_key();
     let encoder = Encoder::new(key, 512, Box::new(payload));
 
-    let put_url = format!("{}{}", upstream_base_url.get_ref(), req.uri());
+    let put_url = create_url(upstream_base_url.get_ref(), &req.uri());
 
     client.put(put_url)
         .header("User-Agent", "Actix-web")
@@ -41,9 +47,10 @@ fn fetch(
     req: HttpRequest,
     client: web::Data<Client>,
     upstream_base_url: web::Data<String>,
+    noop: web::Data<bool>,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
 
-    let get_url = format!("{}{}", upstream_base_url.get_ref(), req.uri());
+    let get_url = create_url(upstream_base_url.get_ref(), &req.uri());
 
     client.get(get_url)
         .header("User-Agent", "Actix-web")
@@ -53,7 +60,7 @@ fn fetch(
             println!("{:?}", e);
             Error::from(e)
         })
-    .map(|res| {
+    .map(move |res| {
         let mut client_resp = HttpResponse::build(res.status());
         for (header_name, header_value) in
             res.headers().iter().filter(|(h, _)| *h != "connection")
@@ -62,18 +69,22 @@ fn fetch(
             }
 
         let key = build_key();
-        let decoder = Decoder::new(key, 512, Box::new(res));
-
-        client_resp.streaming(decoder)
+        if *noop.get_ref() {
+            client_resp.streaming(res)
+        } else {
+            let decoder = Decoder::new(key, 512, Box::new(res));
+            client_resp.streaming(decoder)
+        }
     })
 }
 
 
-pub fn main(listen_addr: &str, listen_port: u16, upstream_base_url: String) -> std::io::Result<()> {
+pub fn main(listen_addr: &str, listen_port: u16, upstream_base_url: String, noop: bool) -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .data(actix_web::client::Client::new())
             .data(upstream_base_url.clone())
+            .data(noop)
             .wrap(middleware::Logger::default())
             .service(web::resource(".*").guard(guard::Get()).to_async(fetch))
             .default_service(web::route().guard(guard::Put()).to_async(forward))
