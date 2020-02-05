9196584de79a392b4a92fca6a458b52e3c2051d4
simon lehericey <mail@simon.lehericey.net>
2020-02-05 14:50:30 +0100

    update file encoder (badly does not use stream no more because of missing api)

--- src/file.rs
+++ src/file.rs
@@ -1,44 +1,44 @@
 use super::config::*;
 use super::decoder::*;
 use super::encoder::*;
-use futures::future::Future;
-use futures::stream::Stream;
-use futures_fs::FsPool;
+use actix_web::Error;
+use bytes::{BufMut, Bytes, BytesMut};
+use futures::executor::block_on_stream;
 
 pub fn encrypt(config: Config) {
-    let fs = FsPool::default();
+    let input: Vec<u8> = std::fs::read(config.input_file.unwrap()).unwrap();
 
-    // our source file
-    let read = fs.read(config.input_file.unwrap(), Default::default());
+    let source: Result<Bytes, Error> = Ok(Bytes::from(input));
+    let source_stream = futures::stream::once(Box::pin(async { source }));
 
-    let encoder = Encoder::new(config.key, config.chunk_size, Box::new(read));
+    let encoder = Encoder::new(config.key, config.chunk_size, Box::new(source_stream));
 
-    // default writes options to create a new file
-    let write = fs.write(config.output_file.unwrap(), Default::default());
+    let buf = block_on_stream(encoder).map(|r| r.unwrap()).fold(
+        BytesMut::with_capacity(64),
+        |mut acc, x| {
+            acc.put(x);
+            acc
+        },
+    );
 
-    // block this thread!
-    // the reading and writing however will happen off-thread
-    encoder
-        .forward(write)
-        .wait()
-        .expect("IO error piping foo.txt to out.txt");
+    std::fs::write(config.output_file.unwrap(), &buf[..]).unwrap();
 }
 
 pub fn decrypt(config: Config) {
-    let fs = FsPool::default();
+    let input: Vec<u8> = std::fs::read(config.input_file.unwrap()).unwrap();
 
-    // our source file
-    let read = fs.read(config.input_file.unwrap(), Default::default());
+    let source: Result<Bytes, Error> = Ok(Bytes::from(input));
+    let source_stream = futures::stream::once(Box::pin(async { source }));
 
-    let decoder = Decoder::new(config.key, Box::new(read));
+    let decoder = Decoder::new(config.key, Box::new(source_stream));
 
-    // default writes options to create a new file
-    let write = fs.write(config.output_file.unwrap(), Default::default());
+    let buf = block_on_stream(decoder).map(|r| r.unwrap()).fold(
+        BytesMut::with_capacity(64),
+        |mut acc, x| {
+            acc.put(x);
+            acc
+        },
+    );
 
-    // block this thread!
-    // the reading and writing however will happen off-thread
-    decoder
-        .forward(write)
-        .wait()
-        .expect("IO error piping foo.txt to out.txt");
+    std::fs::write(config.output_file.unwrap(), &buf[..]).unwrap();
 }
