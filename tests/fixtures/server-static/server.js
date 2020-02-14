a9b1c387726d29dd095bdcf65ca139a90de5ddbd
simon lehericey <mail@simon.lehericey.net>
2020-02-14 09:04:45 +0100

    test server: add a chunked function

--- tests/fixtures/server-static/server.js
+++ tests/fixtures/server-static/server.js
@@ -3,12 +3,22 @@ var fs = require('fs');
 
 var express = require('express');
 var app = express();
+const { Readable } = require('stream');
 
 app.put('*', function(req, res) {
   req.pipe(fs.createWriteStream(__dirname + '/uploads/' +req.url));
 
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('OK!');
+});
+
+app.get('/chunked/*', function(req, res){
+  const path = req.url.substr(8)
+
+  const readStream = fs.createReadStream(__dirname + '/uploads/' + path, { highWaterMark: 1 * 1024});
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  readStream.pipe(res);
 });
 
 app.use(express.static(__dirname + '/uploads'));
