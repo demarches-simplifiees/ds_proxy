a81c4b9aa82c5877d5871ac2cf7454eb9d7dd18d
simon lehericey <mail@simon.lehericey.net>
2020-02-17 10:49:36 +0100

    spawning a live proxy for e2e tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,8 @@
 use assert_cmd::prelude::*;
 use assert_fs::prelude::*;
-use std::process::Command;
+use std::process::{ Command, Child, Output };
+use std::path::Path;
+use encrypt::header::{PREFIX, PREFIX_SIZE};
 
 #[test]
 fn encrypt_and_decrypt() {
@@ -148,4 +150,115 @@ fn the_app_crashes_with_an_invalid_password() {
         .env("DS_SALT", salt);
 
     decrypt_cmd.assert().failure();
+}
+
+use std::{thread, time};
+
+
+#[test]
+fn end_to_end_upload_and_download() {
+    /*
+    This test:
+     - spawns a node server that stores uploaded files in tests/fixtures/server-static/uploads/
+     - spawns a ds proxy that uses the node proxy as a storage backend
+     - uploads a file using curl via the DS proxy
+     - checks that said file is encrypted
+     - downloads the encrypted file via the proxy, and checks that its content matches the initial content
+    */
+    let password = "plop";
+    let salt = "12345678901234567890123456789012";
+    let hash_file_arg = "--hash-file=tests/fixtures/password.hash";
+    let chunk_size = "512"; //force multiple pass
+
+    let original_path = "tests/fixtures/computer.svg";
+    let original_bytes = std::fs::read(original_path).unwrap();
+    let uploaded_path = "tests/fixtures/server-static/uploads/victory";
+
+    let temp = assert_fs::TempDir::new().unwrap();
+    let decrypted_file = temp.child("computer.dec.svg");
+    let decrypted_path = decrypted_file.path();
+
+    if Path::new(uploaded_path).exists() {
+        std::fs::remove_file(uploaded_path)
+            .expect(&format!("Unable to remove {} !", uploaded_path.to_owned()));
+    }
+
+    let mut proxy_server = launch_proxy(password, hash_file_arg, salt, chunk_size);
+    let mut node_server = launch_node();
+
+    thread::sleep(time::Duration::from_millis(1000));
+
+    let curl_upload = curl_put(original_path, "localhost:4444/victory");
+    if !curl_upload.status.success() { panic!("unable to upload file !"); }
+
+    let uploaded_bytes = std::fs::read(uploaded_path).expect("uploaded should exist !");
+    assert_eq!(&uploaded_bytes[0..PREFIX_SIZE], PREFIX);
+
+    decrypt(uploaded_path, decrypted_path, hash_file_arg, password, salt, chunk_size);
+    let decrypted_bytes = std::fs::read(decrypted_path).unwrap();
+    assert_eq!(original_bytes, decrypted_bytes);
+
+    let curl_download = curl_get("localhost:4444/victory");
+    assert_eq!(curl_download.stdout, original_bytes);
+
+    let curl_chunked_download = curl_get("localhost:4444/chunked/victory");
+    assert_eq!(curl_chunked_download.stdout, original_bytes);
+
+    proxy_server.kill().expect("killing the proxy server should succeed !");
+    node_server.kill().expect("killing node's upload server should succeed !");
+    temp.close().unwrap();
+}
+
+
+fn launch_proxy(password: &str, hash_file_arg: &str, salt: &str, chunk_size: &str) -> Child {
+    Command::cargo_bin("ds_proxy")
+        .unwrap()
+        .arg("proxy")
+        .arg("--address=localhost:4444")
+        .arg("--upstream-url=http://localhost:3000")
+        .arg(hash_file_arg)
+        .env("DS_PASSWORD", password)
+        .env("DS_SALT", salt)
+        .env("DS_CHUNK_SIZE", chunk_size)
+        .spawn()
+        .expect("failed to execute ds_proxy")
+}
+
+fn launch_node() -> Child {
+    Command::new("node")
+        .arg("tests/fixtures/server-static/server.js")
+        .spawn()
+        .expect("failed to execute node")
+}
+
+fn curl_put(file_path: &str, url: &str) -> Output {
+    Command::new("curl")
+        .arg("-XPUT")
+        .arg(url)
+        .arg("--data-binary")
+        .arg(format!("@{}", file_path))
+        .output()
+        .expect("failed to perform upload")
+}
+
+fn curl_get(url: &str) -> Output {
+    Command::new("curl")
+        .arg("-XGET")
+        .arg(url)
+        .output()
+        .expect("failed to perform download")
+}
+
+fn decrypt(encrypted_path: &str, decrypted_path: &std::path::Path, hash_path: &str, password: &str, salt: &str, chunk_size: &str) -> assert_cmd::assert::Assert {
+    Command::cargo_bin("ds_proxy")
+        .unwrap()
+        .arg("decrypt")
+        .arg(encrypted_path)
+        .arg(decrypted_path)
+        .arg(hash_path)
+        .env("DS_PASSWORD", password)
+        .env("DS_SALT", salt)
+        .env("DS_CHUNK_SIZE", chunk_size)
+        .assert()
+        .success()
 }
