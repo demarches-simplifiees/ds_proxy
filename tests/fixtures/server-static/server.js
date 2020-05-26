2751c3743a3c3bf8991fb5b5d6f4ab30850a6ff5
Pierre de La Morinerie <kemenaran@gmail.com>
2020-05-26 18:23:51 +0200

    fixup! tests: add optional latency to the test node.js server

--- tests/fixtures/server-static/server.js
+++ tests/fixtures/server-static/server.js
@@ -5,6 +5,18 @@ var express = require('express');
 var app = express();
 const { Readable } = require('stream');
 
+// Add extra latency to all requests
+// Usage: node server.js --latency=1000
+let latencyArg = process.argv.slice(2).find(arg => arg.startsWith('--latency='));
+if (latencyArg) {
+  const latency = parseInt(latencyArg.split('=')[1], 10);
+  if (latency > 0) {
+    console.log('Add latency middleware with: ' + latency  + 'ms');
+    let latencyMiddleware = function(req,res,next) { setTimeout(next, latency) };
+    app.use(latencyMiddleware);
+  }
+}
+
 app.put('*', function(req, res) {
   req.pipe(fs.createWriteStream(__dirname + '/uploads/' +req.url));
 
@@ -31,15 +43,5 @@ app.get('/get/400', function(req, res){
   res.end('KO: 400');
 });
 
-// Run `node server.js --latency=1000` to add latency to all request.
-let latencyArg = process.argv.slice(2).find(arg => arg.startsWith('--latency='));
-if (latencyArg) {
-  const latency = toInt(latencyArg.split('=')[1], 10);
-  if (latency > 0) {
-    let latencyMiddleware = function(req,res,next) { setTimeout(next, latency) };
-    app.use(latencyMiddleware);
-  }
-}
-
 app.use(express.static(__dirname + '/uploads'));
-app.listen(3000);
+app.listen(3000, '127.0.0.1');
