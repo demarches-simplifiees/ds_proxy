fe015c00ca653c53693809657e5116dc42793b5c
simon lehericey <mail@simon.lehericey.net>
2022-01-28 21:24:34 +0100

    clean test/cli.rs

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,22 +1,15 @@
 use assert_cmd::prelude::*;
 use assert_fs::prelude::*;
+use assert_fs::TempDir;
+use std::fs::read;
 use std::process::Command;
 
 mod helpers;
 pub use helpers::*;
 
-const PASSWORD: &str = "plop";
-const SALT: &str = "12345678901234567890123456789012";
-const HASH_FILE_ARG: &str = "--hash-file=tests/fixtures/password.hash";
-
 #[test]
 fn encrypt_and_decrypt() {
-    let temp = assert_fs::TempDir::new().unwrap();
-
-    let password = "plop";
-    let salt = "12345678901234567890123456789012";
-    let hash_file_arg = "--hash-file=tests/fixtures/password.hash";
-    let chunk_size = "512"; //force multiple pass
+    let temp = TempDir::new().unwrap();
 
     let encrypted = temp.child("computer.svg.enc");
     let decrypted = temp.child("computer.dec.svg");
@@ -29,64 +22,33 @@ fn encrypt_and_decrypt() {
         .arg("encrypt")
         .arg(COMPUTER_SVG_PATH)
         .arg(encrypted_path)
-        .arg(hash_file_arg)
-        .env("DS_PASSWORD", password)
-        .env("DS_SALT", salt)
-        .env("DS_CHUNK_SIZE", chunk_size);
-
-    encrypt_cmd.assert().success();
+        .arg(HASH_FILE_ARG)
+        .env("DS_PASSWORD", PASSWORD)
+        .env("DS_SALT", SALT)
+        .env("DS_CHUNK_SIZE", CHUNK_SIZE.to_string())
+        .assert()
+        .success();
 
     let mut decrypt_cmd = Command::cargo_bin("ds_proxy").unwrap();
     decrypt_cmd
         .arg("decrypt")
         .arg(encrypted_path)
         .arg(decrypted_path)
-        .arg(hash_file_arg)
-        .env("DS_PASSWORD", password)
-        .env("DS_SALT", salt)
-        .env("DS_CHUNK_SIZE", chunk_size);
+        .arg(HASH_FILE_ARG)
+        .env("DS_PASSWORD", PASSWORD)
+        .env("DS_SALT", SALT)
+        .env("DS_CHUNK_SIZE", CHUNK_SIZE.to_string())
+        .assert()
+        .success();
 
-    decrypt_cmd.assert().success();
-
-    let decrypted_bytes = std::fs::read(decrypted_path).unwrap();
-
-    temp.close().unwrap();
+    let decrypted_bytes = read(decrypted_path).unwrap();
 
     assert_eq!(COMPUTER_SVG_BYTES, decrypted_bytes);
 }
 
 #[test]
 fn decrypt_witness_file() {
-    let temp = assert_fs::TempDir::new().unwrap();
-
-    let password = "plop";
-    let salt = "12345678901234567890123456789012";
-    let hash_file_arg = "--hash-file=tests/fixtures/password.hash";
-
-    let decrypted = temp.child("computer.dec.svg");
-    let decrypted_path = decrypted.path();
-
-    let mut decrypt_cmd = Command::cargo_bin("ds_proxy").unwrap();
-    decrypt_cmd
-        .arg("decrypt")
-        .arg(ENCRYPTED_COMPUTER_SVG_PATH)
-        .arg(decrypted_path)
-        .arg(hash_file_arg)
-        .env("DS_PASSWORD", password)
-        .env("DS_SALT", salt);
-
-    decrypt_cmd.assert().success();
-
-    let decrypted_bytes = std::fs::read(decrypted_path).unwrap();
-
-    temp.close().unwrap();
-
-    assert_eq!(decrypted_bytes, COMPUTER_SVG_BYTES);
-}
-
-#[test]
-fn decrypting_a_plaintext_file_yields_the_original_file() {
-    let temp = assert_fs::TempDir::new().unwrap();
+    let temp = TempDir::new().unwrap();
 
     let decrypted = temp.child("computer.dec.svg");
     let decrypted_path = decrypted.path();
@@ -98,23 +60,41 @@ fn decrypting_a_plaintext_file_yields_the_original_file() {
         .arg(decrypted_path)
         .arg(HASH_FILE_ARG)
         .env("DS_PASSWORD", PASSWORD)
-        .env("DS_SALT", SALT);
+        .env("DS_SALT", SALT)
+        .assert()
+        .success();
 
-    decrypt_cmd.assert().success();
+    let decrypted_bytes = read(decrypted_path).unwrap();
 
-    let decrypted_bytes = std::fs::read(decrypted_path).unwrap();
+    assert_eq!(decrypted_bytes, COMPUTER_SVG_BYTES);
+}
 
-    temp.close().unwrap();
+#[test]
+fn decrypting_a_plaintext_file_yields_the_original_file() {
+    let temp = TempDir::new().unwrap();
+
+    let decrypted = temp.child("computer.dec.svg");
+    let decrypted_path = decrypted.path();
+
+    let mut decrypt_cmd = Command::cargo_bin("ds_proxy").unwrap();
+    decrypt_cmd
+        .arg("decrypt")
+        .arg(ENCRYPTED_COMPUTER_SVG_PATH)
+        .arg(decrypted_path)
+        .arg(HASH_FILE_ARG)
+        .env("DS_PASSWORD", PASSWORD)
+        .env("DS_SALT", SALT)
+        .assert()
+        .success();
+
+    let decrypted_bytes = read(decrypted_path).unwrap();
 
     assert_eq!(decrypted_bytes, COMPUTER_SVG_BYTES);
 }
 
 #[test]
 fn the_app_crashes_on_a_missing_password() {
-    let temp = assert_fs::TempDir::new().unwrap();
-
-    let salt = "12345678901234567890123456789012";
-    let hash_file_arg = "--hash-file=tests/fixtures/password.hash";
+    let temp = TempDir::new().unwrap();
 
     let decrypted = temp.child("computer.dec.svg");
     let decrypted_path = decrypted.path();
@@ -124,18 +104,15 @@ fn the_app_crashes_on_a_missing_password() {
         .arg("proxy")
         .arg(ENCRYPTED_COMPUTER_SVG_PATH)
         .arg(decrypted_path)
-        .arg(hash_file_arg)
-        .env("DS_SALT", salt);
+        .arg(HASH_FILE_ARG)
+        .env("DS_SALT", SALT);
 
     decrypt_cmd.assert().failure();
 }
 
 #[test]
 fn the_app_crashes_on_a_missing_hash() {
-    let temp = assert_fs::TempDir::new().unwrap();
-
-    let password = "plop";
-    let hash_file_arg = "--hash-file=tests/fixtures/password.hash";
+    let temp = TempDir::new().unwrap();
 
     let decrypted = temp.child("computer.dec.svg");
     let decrypted_path = decrypted.path();
@@ -145,15 +122,15 @@ fn the_app_crashes_on_a_missing_hash() {
         .arg("proxy")
         .arg(ENCRYPTED_COMPUTER_SVG_PATH)
         .arg(decrypted_path)
-        .arg(hash_file_arg)
-        .env("DS_PASSWORD", password);
+        .arg(HASH_FILE_ARG)
+        .env("DS_PASSWORD", PASSWORD);
 
     decrypt_cmd.assert().failure();
 }
 
 #[test]
 fn the_app_crashes_with_an_invalid_password() {
-    let temp = assert_fs::TempDir::new().unwrap();
+    let temp = TempDir::new().unwrap();
 
     let password = "this is not the expected password";
 
