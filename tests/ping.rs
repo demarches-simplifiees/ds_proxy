b623d80b3278ff2b3876d4b021aabc494e47296c
simon lehericey <mail@simon.lehericey.net>
2022-01-29 15:31:03 +0100

    small cleaning on ping test

--- tests/ping.rs
+++ tests/ping.rs
@@ -1,5 +1,5 @@
 use serial_test::serial;
-use std::path::Path;
+use std::fs::File;
 use std::{thread, time};
 
 mod helpers;
@@ -16,8 +16,8 @@ fn ping() {
      - curl /ping and expect to fetch a 404 which should trigger a maintenance mode
        on a upper stream proxy
     */
-    let mut proxy_server = launch_proxy(PrintServerLogs::No);
-    thread::sleep(time::Duration::from_millis(2000));
+    let _proxy_server = launch_proxy(PrintServerLogs::No);
+    thread::sleep(time::Duration::from_secs(2));
 
     let maintenance_file_path = "maintenance";
 
@@ -25,15 +25,10 @@ fn ping() {
 
     assert_eq!(curl_get_status("localhost:4444/ping"), "200");
 
-    std::fs::File::create(maintenance_file_path)
-        .unwrap_or_else(|_| panic!("Unable to create {} !", maintenance_file_path.to_owned()));
+    File::create(maintenance_file_path)
+        .unwrap_or_else(|_| panic!("Unable to create {} !", maintenance_file_path));
 
     assert_eq!(curl_get_status("localhost:4444/ping"), "404");
 
     ensure_is_absent(maintenance_file_path);
-
-    proxy_server
-        .child
-        .kill()
-        .expect("killing the proxy server should succeed !");
 }
