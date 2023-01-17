30309102f0a0ef80cd75743df4bf32387ed34f77
simon lehericey <mail@simon.lehericey.net>
2023-01-17 15:40:06 +0100

    HEADER_v2_SIZE

--- src/crypto/header.rs
+++ src/crypto/header.rs
@@ -3,10 +3,12 @@ use std::convert::TryInto;
 
 pub const PREFIX: &[u8] = b"J'apercus l'audacieux capitaine.";
 pub const PREFIX_SIZE: usize = 32;
-const VERSION_NB: usize = 1;
-const VERSION_NB_SIZE: usize = 8;
+const VERSION_NB: usize = 2;
+pub const VERSION_NB_SIZE: usize = 8;
 const CHUNK_SIZE_SIZE: usize = 8; //usize size
+const KEY_ID_SIZE: usize = 8; //u64 size
 pub const HEADER_SIZE: usize = PREFIX_SIZE + VERSION_NB_SIZE + CHUNK_SIZE_SIZE;
+pub const HEADER_V2_SIZE: usize = HEADER_SIZE + KEY_ID_SIZE;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct Header {
