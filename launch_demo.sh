b8786d915129a6e62f8dbaa165ad0929a693cf54
simon lehericey <mail@simon.lehericey.net>
2025-11-13 12:22:42 +0100

    fix: launch demo in aws mode use env var for AWS_SECRET_KEY, AWS_ACCESS_KEY, AWS_REGION, UPSTREAM

--- launch_demo.sh
+++ launch_demo.sh
@@ -18,10 +18,12 @@ echo 'building keyring file'
 
 if [ "$1" = "aws" ]; then
   echo 'launching ds_proxy in aws mode listenning on real s3 backend'
-  RUST_LOG=info ./target/release/ds_proxy proxy --address "127.0.0.1:4444" --password-file <(echo -n "$PASSWORD") --salt "$SALT" --keyring-file "$KEYRING_FILE" --upstream-url "https://test-de-proxy.s3-eu-west-1.amazonaws.com" --aws-access-key $ACCESS_KEY --aws-secret-key "plop" --aws-region "eu-west-1" > "$DS_PROXY_LOG" 2>&1 &
+  RUST_LOG=info ./target/release/ds_proxy proxy --address "127.0.0.1:4444" --password-file <(echo -n "$PASSWORD") --salt "$SALT" --keyring-file "$KEYRING_FILE" --upstream-url "$UPSTREAM_URL" --aws-access-key "$AWS_ACCESS_KEY" --aws-secret-key "$AWS_SECRET_KEY" --aws-region "$AWS_REGION" > "$DS_PROXY_LOG" 2>&1 &
+
 elif [ "$1" = "fake_aws" ]; then
   echo 'launching ds_proxy in aws mode listenning on 4444 binded on node server'
-  RUST_LOG=info ./target/release/ds_proxy proxy --address "127.0.0.1:4444" --password-file <(echo -n "$PASSWORD") --salt "$SALT" --keyring-file "$KEYRING_FILE" --upstream-url "http://localhost:3333" --aws-access-key "$ACCESS_KEY" --aws-secret-key "$SECRET_KEY" --aws-region "eu-west-1" > "$DS_PROXY_LOG" 2>&1 &
+  RUST_LOG=info ./target/release/ds_proxy proxy --address "127.0.0.1:4444" --password-file <(echo -n "$PASSWORD") --salt "$SALT" --keyring-file "$KEYRING_FILE" --upstream-url "http://localhost:3333" --aws-access-key "$AWS_ACCESS_KEY" --aws-secret-key "$AWS_SECRET_KEY" --aws-region "eu-west-1" > "$DS_PROXY_LOG" 2>&1 &
+
 else
   echo 'launching ds_proxy listenning on 4444 binded on node server, using redis to emulate write once'
   if nc -z localhost $REDIS_PORT 2>/dev/null; then
